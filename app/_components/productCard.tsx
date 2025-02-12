d4324acef332d8f3617146de72004e06a7424764
monire91 <aglida1370@gmail.com>
2025-02-12 11:09:13 -0600

    separated product card to a component and used it

---
+++ app/_components/productCard.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { Product } from "@/types";
+import Image from "next/image";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <div className="border p-4 rounded-lg">
+      <Image
+        src={product.images[product.colors[0]]}
+        alt={product.name}
+        width={300}
+        height={200}
+        className="w-full h-48 object-cover mb-2"
+      />
+
+      {/* Product Title & Price */}
+      <h2 className="text-xl font-semibold">{product.name}</h2>
+      <p className="text-gray-600">{product.description}</p>
+      <p className="text-lg font-bold">${product.price}</p>
+
+      {/* Colors Display */}
+      <div className="mt-2 flex space-x-2">
+        {product.colors.map((color) => (
+          <div
+            key={color}
+            className="w-5 h-5 rounded-full border"
+            style={{ backgroundColor: color }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Product } from "@/types";
 import { useEffect, useState } from "react";
+import ProductCard from "./_components/productCard";
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -8,32 +9,16 @@ export default function Home() {
   useEffect(() => {
     // Fetch products from the API
     fetch("/api/products")
-      .then((response) => {
-        console.log("response", response);
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
 
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Products</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
         {products.map((product) => (
-          <div key={product.id} className="border p-4 rounded-lg">
-            <img
-              src={product.images[product.colors[0]]} // Get the image for the first color
-              alt={product.name}
-              className="w-full h-48 object-cover mb-2"
-            />
-            <h2 className="text-xl font-semibold">{product.name}</h2>
-            <p className="text-gray-600">{product.description}</p>
-            <p className="text-lg font-bold">${product.price}</p>
-            <div className="mt-2">
-              {/* <p className="text-sm">Sizes: {product.sizes.join(", ")}</p> */}
-              {/* <p className="text-sm">Colors: {product.colors.join(", ")}</p> */}
-            </div>
-          </div>
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
