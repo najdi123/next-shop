f709cb559138ccd41926e291bf2bc1cf3f4f7f67
monire91 <aglida1370@gmail.com>
2025-02-12 12:59:47 -0600

    added api to get product by id

---
+++ app/api/product/[id]/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+    try {
+
+        const { id } = await params;
+        const filePath = path.join(process.cwd(), "data", "products.json");
+
+        const fileContents = fs.readFileSync(filePath, "utf8");
+        const products = JSON.parse(fileContents);
+
+
+        // Find the product by ID
+        const product = products.find((p: { id: number }) => p.id === Number(id));
+
+        if (!product) {
+            console.error("❌ Product not found for ID:", id);
+            return NextResponse.json({ error: "Product not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(product, { status: 200 });
+    } catch (error) {
+        console.error("❌ Error reading product:", error);
+        return NextResponse.json({ error: "Failed to fetch product" }, { status: 500 });
+    }
+}
